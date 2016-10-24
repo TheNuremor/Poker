61462a97c4146a595d6edc8d5c9f748f77605eb2
Pascal <pascal.grosmann@googlemail.com>
2016-10-24 16:50:38 +0200

    3.

--- src/com/company/Table.java
+++ src/com/company/Table.java
@@ -1,8 +1,15 @@
 package com.company;
 
 public class Table {
+    //Pot
     int pot=0;
 
+    //TableStack
 
+    //Methoden
+    //Verteilung Dealer und Blinds
+    //Verteilung Sitzplätze
+    //Zuweisung wer dran ist
+    //Spiel vorbei->Auswertung
 }
 
