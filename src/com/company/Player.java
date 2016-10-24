24f66655821726502f84791408266d217c05e00f
Pascal <pascal.grosmann@googlemail.com>
2016-10-24 18:18:54 +0200

    3.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,14 +1,40 @@
 package com.company;
 
+import java.util.zip.CheckedInputStream;
+
 public class Player {
-    //Konto
+
+    public String name;
+    public int cash;
+
+
+
+    public Player(){
+        this.name = name;
+        this.cash = cash;
+    }
+
+
+
+
     //HandStack
     //Dealer,Small, Big Blind
     //Sitzplatz
-    //Name
+
 
 
     //Methoden(Interaktionen)
-
+        //Raise
+            //Betrag
+        //Bet
+            //Betrag
+        //Check
+            //Betrag von vorher
+        //Call
+        //Fold
+            //Exit für Runde
+        //All in
+            //Sieg -> Pot
+            //Verliert -> Exit Game
 }
 
