7977ba1bf865a678fb14694f0303744adcb0197d
Iwanosch1 <Blitzmerker1>
2016-11-18 11:42:49 +0100

    pokercard implemented

--- src/com/company/CardStack.java
+++ src/com/company/CardStack.java
@@ -6,41 +6,34 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-class CardStack {
+public class CardStack {
 
-    List<PokerCard> cards;
+    public List<Card> cards;
 
     //Constructor
-    CardStack(int STACK_SIZE) {
+    public CardStack(int STACK_SIZE) {
         cards = new ArrayList<>(STACK_SIZE);
     }
 
     //Constructor with shuffle
-    CardStack() {
+    public CardStack() {
         this(52);
         for (PokerCard.Color color : PokerCard.Color.values()) {
             for (PokerCard.Value value : PokerCard.Value.values()) {
                 cards.add(new Card(value, color));
-                //cards.add(new Card(PokerCard.Value.ASS, PokerCard.Color.DIAMONDS));
                 //System.out.printf("%s %s\n",color.name(), value.name());
             }
         }
-
         Collections.shuffle(cards);
     }
 
-    List<PokerCard> getCards() {
-        return cards;
-    }
-
-    PokerCard deal() {
+    public Card deal(){
         return cards.remove(0);
     }
 
-    @Override
     public String toString() {
         String output = "";
-        for (PokerCard c : cards) {
+        for (Card c : cards) {
             output += c.toString() + "\n";
         }
         return output;
