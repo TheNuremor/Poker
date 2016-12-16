1563ced42419074b736f30a86c6018ab73cc70b4
TheNuremor <pascal.grosmann@googlemail.com>
2016-12-16 11:44:30 +0100

    login created and many many fixes

--- src/com/company/ServerThread.java
+++ src/com/company/ServerThread.java
@@ -6,29 +6,51 @@ import java.net.Socket;
 
 public class ServerThread extends Thread {
 
-    private Socket client;
+    private Socket clientSocket;
+    public Socket client;
+    public String str = null;
+    Table table = new Table();
 
-    public ServerThread (Socket accept) {
-        client = accept;
+
+
+    public ServerThread (Socket client, ServerSocket server, boolean gameRunning) {
+        server = server;
+        client = client;
+        gameRunning =gameRunning;
     }
-
 
 
     @Override
     public void run(){
+        login();
+
+    }
+
+    public void login(){
         try {
+
             OutputStream out = client.getOutputStream();
             PrintStream output  = new PrintStream(out, true);
 
             InputStream in = client.getInputStream();
-            System.out.println("Verfügbare Bytes: " + in.available());
             BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
             System.out.println("Willkommen auf dem Server :)");
-            login();
+            System.out.println("\n\tBitte geben sie ihren Namen ein:\n");
 
+            str = input.readLine();
+            System.out.printf("\n\n\tIhr Name ist:", str);
 
+            if (!gameRunning) {
+                Player player = new Player();
+                player.setName(str);
+                table.addPlayer(player, table.playerList);
 
-            System.out.println("Hello World");
+                System.out.printf("Der Spieler %s wurde zur Spielerliste hinzugefügt!", str);
+            }else {
+                System.out.println("Es sind schon zu viele Spieler im Spiel.");
+            }
+            server.checkLogin();
 
         } catch (IOException e) {
             System.out.println("IOException in run()...");
@@ -36,9 +58,11 @@ public class ServerThread extends Thread {
             close();
         }
     }
-    public void login(){
+
+    public void sendDate(){
 
     }
+
 
     public void close(){
 
