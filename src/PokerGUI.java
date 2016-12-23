8314a49a6440edba57444f23b6c2ca52ef59b234
TheNuremor <pascal.grosmann@googlemail.com>
2016-12-23 10:22:35 +0100

    GUI

---
+++ src/PokerGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Pascal on 23.12.2016.
+ */
+public class PokerGUI {
+    private JButton button1;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+}
